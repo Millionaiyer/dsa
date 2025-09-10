7b04d0b8818155e4c1292be74fde850a3e1007ca
millionaiyer <surajsaiyer@gmail.com>
2025-09-10 14:13:54 +0000

    second largest updated

--- dsa/index.js
+++ dsa/index.js
@@ -1,16 +1,26 @@
 // find the 2nd largest number in an array
 
-function secondLargest(arr) {
-  let largest = -Infinity;
-  let second = 0;
+// function secondLargest(arr) {
+//   let largest = -Infinity;
+//   let second = 0;
 
-  for (i = 0; i < arr.length; i++) {
-    if (arr[i] > largest) {
-      second = largest;
-      largest = arr[i];
-    }
+//   for (i = 0; i < arr.length; i++) {
+//     if (arr[i] > largest) {
+//       second = largest;
+//       largest = arr[i];
+//     } else if (arr[i] > second && arr[i] != largest) {
+//       second = arr[i];
+//     }
+//   }
+//   return second;
+// }
+
+// console.log(secondLargest([2, 1, 6, 82, 1]));
+
+//
+
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 3; j++) {
+    console.log("i = " + i + " j = " + j);
   }
-  return second;
 }
-
-console.log(secondLargest([2, 1, 6, 82, 1]));
